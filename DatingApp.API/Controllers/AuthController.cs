0f787f423b48acbab8eafe3962632660c38f0216
Csaba Nagy <ncsabak@gmail.com>
2018-10-08 20:00:53 +0200

    Token Authentication/ Login method in API

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -12,10 +18,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
-
+            _config = config;
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
@@ -24,7 +32,7 @@ namespace DatingApp.API.Controllers
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
             if(await  _repo.UserExists(userForRegisterDto.Username) )
-                return BadRequest("Username laready exists");
+                return BadRequest("Username already exists");
 
             var userToCreate = new User
             {
@@ -33,6 +41,42 @@ namespace DatingApp.API.Controllers
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
             return StatusCode(201);
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            //Ellenőrzi hogy van e ilyen ember az adatbázisban
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            
+            if(userFromRepo == null)
+                return Unauthorized();
+            //Ezeket kell tartalmazni a token-nek
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username.ToString())
+            };
+            //Kiolvassa a konfigból a saját kulcsot és abból generál key-t
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            //kódoljuk a kulcsot sha512-vel
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            //Token leírást csinál
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //Token generálás
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new {
+                //
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
