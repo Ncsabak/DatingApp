be030b6d5ecf56e127403920d7dc6437177adf13
Csaba Nagy <ncsabak@gmail.com>
2018-10-09 20:22:15 +0200

    login and get token back

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Successfully');
+    }, error => {
+        console.log('Failed to login');
+    });
   }
 
 }
