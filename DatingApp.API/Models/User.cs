0f9ddd2d82128f0fb13a53b6f62d743cbbbed7d3
Csaba Nagy <ncsabak@gmail.com>
2018-10-27 21:26:35 +0200

    Extending the API (user data)

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace DatingApp.API.Models
 {
     public class User
@@ -5,7 +9,18 @@ namespace DatingApp.API.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBrith { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Inroduction { get; set; }
+        public string LokingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
